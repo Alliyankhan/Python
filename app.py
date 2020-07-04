0c19c7157c20d87e397eefa6afbf5d709f83aba2
Alliyankhan <64958767+Alliyankhan@users.noreply.github.com>
2020-07-04 13:52:48 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,73 @@
+# print("Hello World")
+# age = int(input('What You Like?'))
+# if age > 18 :
+#   print ('You are lucky')
+# else :
+#   print ('We are same bro')
+# species = input('Animal')
+# if species == "cat":
+#   print("Yep, it's cat.")
+# else :
+#   print ('Wrong Answer')
+# city_0 = "Karachi"
+# city_1 = "Lahore "
+# city_2 = "Islamabad"
+# city_3 = "Faisalabad"
+# city_4 = "Peshawar"
+# city_5 = "Qutta"
+# print("Welcome to " + city_0)
+# 
+# 
+# val1 = int(input('Enter the first value'))
+# val2 = int(input('Enter the second value'))
+# operator = input ('Enter operator')
+
+# if operator == '+':
+#       val = val1 + val2
+#       print (val, 'answer')
+# elif operator == '-':
+#       val = val1 - val2
+#       print (val1, 'answer')
+# elif operator == '*':
+#       val = val1 * val2
+#       print (val, 'answer')
+# elif operator == '/':
+#       val = val1 / val2
+#       print (val, 'answer')
+# else :
+#       print ('Enter Correct Operator')
+  
+# first_names = ["BlueRay ", "Upchuck ", "Lojack ",
+# "Gizmo ", "Do-Rag "]
+# last_names = ["Zzz", "Burp", "Dogbone", "Droop"]
+# full_names = []
+# for a_first_name in first_names:
+#   for a_last_name in last_names:
+#    full_names.append(a_first_name + " " +
+#    a_last_name)
+#    print (full_names)
+
+# name = 'Junaid'
+# age = str(20)
+# job = 'Graphic Designer'
+# print ('{} is a well experienced person. He is {} year old. He is a Professional {}').format("name, age, job" )
+# print (name + ' is a well experienced person. He is '+ age + ' year old. He is a Professional ' + job )
+
+# customer = {"first name": "David",
+# "last name": "Elliott",
+# "address": "4803 Wellesley St."}
+# print(customer["address"])
+
+import datetime
+
+datetime_object = datetime.datetime.now()
+print(datetime_object)
+
+
+
+
+
+
+
+
+
